acf49b88c5a09f745ff68fbf537eeaba5bbfbcd7
Gmicheletti <guilherme_micheletti@hotmail.com>
2023-11-02 13:15:37 -0300

    add action button

--- src/main/java/com/arroba/ui/arrobaUI.java
+++ src/main/java/com/arroba/ui/arrobaUI.java
@@ -2,6 +2,8 @@ package com.arroba.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class arrobaUI extends JFrame {
@@ -26,14 +28,16 @@ public class arrobaUI extends JFrame {
     JLabel logoArroba = new JLabel(new ImageIcon("./src/main/resources/img/logorz.png"));    //LOGOS DO SISTEMA
 
     public arrobaUI(){
+
         this.setTitle("arrob@");
         this.setSize(600,600);
+
+
         windowLogin.setLayout(new FlowLayout(FlowLayout.CENTER,200,50));
         windowLogin.setBackground(new Color(255, 255, 255));
         divUser.setBackground(new Color(255, 255, 255));
         divPassword.setBackground(new Color(255, 255, 255));
         divCredential.setBackground(new Color(255, 255, 255));
-
 
         windowLogin.add(logoArroba);
 
@@ -52,12 +56,18 @@ public class arrobaUI extends JFrame {
         windowLogin.add(loginButton);
         windowLogin.add(loginCreateAccount);
 
-
-
         this.getContentPane().add(windowLogin);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
+
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                windowLogin.setVisible(false);
+            }
+        });
 
 
     }
