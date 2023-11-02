bd968ee2aed0c5cc9526316c2d7d805e86580ed6
Gmicheletti <guilherme_micheletti@hotmail.com>
2023-11-02 17:10:31 -0300

    update ui

--- src/main/java/com/arroba/ui/HomeUI.java
+++ src/main/java/com/arroba/ui/HomeUI.java
@@ -10,9 +10,8 @@ public class HomeUI extends JFrame {
     //ELEMENTS LOGIN
     private JPanel body = new JPanel();
     private JPanel sectionHome = new JPanel();
-
     private JLabel jLabelTitulo = new JLabel("Home"); //MENSAGEM
-
+    private JButton previousButton = new JButton("Voltar");
 
 
     public static void main(String[] args) {
@@ -20,20 +19,16 @@ public class HomeUI extends JFrame {
     }
     public HomeUI(){
 
-
-
         sectionHome.setLayout(new GridLayout(3, 3, 20, 25));
         sectionHome.setBackground(new Color(255, 255, 255));
         sectionHome.add(jLabelTitulo);
-
+        sectionHome.add(previousButton);
 
         body.setLayout(new FlowLayout(FlowLayout.CENTER,200,190));
         body.setBackground(new Color(255, 255, 255));
         body.add(sectionHome);
 
-
-
-        this.setTitle("arrob@");
+        this.setTitle("arrob@ - Home");
         this.setSize(1100,800);
         this.getContentPane().add(body);
         this.setLocationRelativeTo(null);
@@ -41,8 +36,14 @@ public class HomeUI extends JFrame {
         this.setVisible(true);
 
 
+        previousButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-
+                LoginUI loginUI = new LoginUI();
+                dispose();
+            }
+        });
 
     }
 
