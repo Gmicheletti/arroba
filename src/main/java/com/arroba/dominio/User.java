5c890e7dbe5c5290dcafe0cdd91a61d7482258bb
Lucas Rodrigues Dias Nascimento <145167515+lucasrodriguesdias@users.noreply.github.com>
2023-11-11 14:37:58 -0300

    update back

--- src/main/java/com/arroba/dominio/User.java
+++ src/main/java/com/arroba/dominio/User.java
@@ -1,46 +1,33 @@
 package com.arroba.dominio;
 
-import java.util.Date;
 import java.util.Objects;
 
 public class User {
     private String nome;
-    private String sobrenome;
+    private String nascimento;
     private String email;
     private String senha;
-    private Integer telefone;
-    private char sexo;
-    private Date nascimento;
-    private String nacionalidade;
+    private User amigo;
+
 
     public User() {
     }
 
-    public User(String nome, String sobrenome, String email, String senha, Integer telefone, char sexo, Date nascimento, String nacionalidade) {
+    public User(String nome, String email, String senha, String nascimento) {
         this.nome = nome;
-        this.sobrenome = sobrenome;
         this.email = email;
         this.senha = senha;
-        this.telefone = telefone;
-        this.sexo = sexo;
         this.nascimento = nascimento;
-        this.nacionalidade = nacionalidade;
     }
+
+
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome() {
+    public void setNome(String s) {
         this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
     }
 
     public String getEmail() {
@@ -59,36 +46,12 @@ public class User {
         this.senha = senha;
     }
 
-    public Integer getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(Integer telefone) {
-        this.telefone = telefone;
-    }
-
-    public char getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public Date getNascimento() {
+    public String getNascimento() {
         return nascimento;
     }
 
-    public void setNascimento(Date nascimento) {
+    public void setNascimento(String nascimento) {
         this.nascimento = nascimento;
-    }
-
-    public String getNacionalidade() {
-        return nacionalidade;
-    }
-
-    public void setNacionalidade(String nacionalidade) {
-        this.nacionalidade = nacionalidade;
     }
 
     @Override
@@ -108,19 +71,19 @@ public class User {
     public String toString() {
         return "User{" +
                 "nome='" + nome + '\'' +
-                ", sobrenome='" + sobrenome + '\'' +
+                ", nascimento=" + nascimento +
                 ", email='" + email + '\'' +
                 ", senha='" + senha + '\'' +
-                ", telefone=" + telefone +
-                ", sexo=" + sexo +
-                ", nascimento=" + nascimento +
-                ", nacionalidade='" + nacionalidade + '\'' +
                 '}';
     }
 
-    public void registerUser (String nome, String sobrenome, String email, String senha, Integer telefone, char sexo, Date nascimento, String nacionalidade){
-        //Tela de cadastro
-//        System.in = setNome();
+    public boolean checkUser() {
+        User[] userList = new User[0];
+        for (User user : userList) {
+            if (user.getEmail().equals(email) && user.getSenha().equals(senha)) {
+                return true; // Encontrou um usuário correspondente
+            }
+        }
+        return false; // Não
     }
-
 }
